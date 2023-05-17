3e07db582924f783763116c430eed10170476661
Franck Owen <fxfrancky@yahoo.com>
2023-05-17 23:05:09 +0200

    fixing cache rtk issues

--- place2connect-ui/vite.config.js
+++ place2connect-ui/vite.config.js
@@ -4,8 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
+    proxy: {
+      '/api': {
+        forward: 'https://api.place2connect.com',
+        secure: true,
+        ws: true,
+        rewrite: path => path.replace('api', ''),
+      },
+    },
     host: true,
     port: 3000,
+    
   },
   plugins: [react()],
 })
