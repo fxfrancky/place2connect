f23de5024b234ff9f93f09b8f36e6709853d9aac
Franck Owen <fxfrancky@yahoo.com>
2023-05-17 14:04:59 +0200

    redux cache

--- place2connect-ui/src/scenes/profilePage/index.jsx
+++ place2connect-ui/src/scenes/profilePage/index.jsx
@@ -1,31 +1,35 @@
 import { Box, useMediaQuery } from "@mui/material";
-import { useParams } from "react-router-dom";
+import { useParams, useState, useEffect } from "react-router-dom";
 import Navbar from "../../scenes/navbar";
 import FriendListWidget from "../../scenes/widgets/FriendListWidget";
 import MyPostWidget from "../../scenes/widgets/MyPostWidget";
 import PostsWidget from "../../scenes/widgets/PostsWidget";
 import UserWidget from "../../scenes/widgets/UserWidget";
-import {useGetUserQuery} from '../../features/users/usersApiSlice'
+import { selectCurrentToken } from '../../features/auth/authSlice';
+import { useSelector } from "react-redux";
+// import {useGetUserQuery} from '../../features/users/usersApiSlice'
+const GO_API =  import.meta.env.VITE_REACT_APP_BACKEND;
 
 const ProfilePage = () => {
-  // const [user, setUser] = useState(null);
+  const [user, setUser] = useState(null);
   const { userID } = useParams();
   const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
+  const token = useSelector(selectCurrentToken)
    // Get the user By ID
-  const {
-  data:user,
-  isSuccess,
-  isError,
-  error,
-  isLoading
-} = useGetUserQuery(userID) 
+//   const {
+//   data:user,
+//   isSuccess,
+//   isError,
+//   error,
+//   isLoading
+// } = useGetUserQuery(userID) 
 
   // setUser(response)
 
-  if (!user){
-    console.log(`user not found with id `, userID)
-    return null;
-  }
+  // if (!user){
+  //   console.log(`user not found with id `, userID)
+  //   return null;
+  // }
 
   // const getUser = async () => {
   //   const response = await fetch(`http://localhost:3001/users/${userId}`, {
@@ -39,15 +43,28 @@ const ProfilePage = () => {
   // useEffect(() => {
   //   getUser();
   // }, []); // eslint-disable-line react-hooks/exhaustive-deps
-  console.log('IS Success', isSuccess)
-  console.log('IS Error', isError, error)
-  console.log('IS loading', isLoading)
+  // console.log('IS Success', isSuccess)
+  // console.log('IS Error', isError, error)
+  // console.log('IS loading', isLoading)
  
-  {isError && (<p>Saddly an error occured {error.message}</p>)}
-  {isLoading && (<p>Wait a minute. The page is still loading</p>)}
+  // {isError && (<p>Saddly an error occured {error.message}</p>)}
+  // {isLoading && (<p>Wait a minute. The page is still loading</p>)}
+
+  const getUser = async () => {
+  const response = await fetch(`${GO_API}/users/${userID}`, {
+    method: "GET",
+    headers: { Authorization: `Bearer ${token}` },
+  });
+    const data = await response.json();
+    console.log("******************************** the user returned is ", data)
+    setUser(data);
+  };
+
+  useEffect(() => {
+    getUser();
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   if (!user) return null;
-
 
   return ( <Box>
       <Navbar />
@@ -58,7 +75,7 @@ const ProfilePage = () => {
         gap="2rem"
         justifyContent="center"
       >
-        { isSuccess && (
+        (
           <>
           <Box flexBasis={isNonMobileScreens ? "26%" : undefined}>
           <UserWidget userID={userID} picturePath={user.picturePath} />
@@ -74,7 +91,7 @@ const ProfilePage = () => {
           <PostsWidget userID={userID} isProfile />
         </Box>
         </>
-        )}  
+        ) 
       </Box>
     </Box>
   );}
